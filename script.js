6e417f96dfb0101d65cf3aed215060f7a0380041
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-18 21:54:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const data = [
+    { name: "john", age: 24, profession: "developer" },
+    { name: "jane", age: 27, profession: "admin" },
+  ];
+  
+  // 1. Print Developers
+  function printDeveloper() {
+    for( var temp of data){
+        if( temp.profession == 'developer'){
+            console.log(temp.name);
+        }
+    }
+  }
+  
+  // 2. Add Data
+  function addData(newData) {
+       data.push(newData);
+  }
+  
+  // 3. Remove Admins
+  function removeAdmin() {
+    
+    for( var temp of data){
+        if( temp.profession == 'admin'){
+            data.reduce(temp);
+        }
+    }
+  }
+  
+  // 4. Concatenate Array
+  function concatenateArray() {
+    
+  }
+  
+  // 5. Average Age
+  function averageAge() {}
+  
+  // 6. Age Check
+  function checkAgeAbove25() {}
+  
+  // 7. Unique Professions
+  function uniqueProfessions() {}
+  
+  // 8. Sort by Age
+  function sortByAge() {}
+  
+  // 9. Update Profession
+  function updateJohnsProfession() {}
+  
+  // 10. Profession Count
+  function getTotalProfessions() {}
